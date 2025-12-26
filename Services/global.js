42e15a5ed8b09e66f81f7dc8097909cbf91a1786
Pranav Nanegaonkar <nanegaonkarpranav68@gmail.com>
2025-12-26 23:43:54 +0530

    commant out the AWS s3 bucket

--- Services/global.js
+++ Services/global.js
@@ -105,11 +105,11 @@ exports.fileUpload = function (req, res) {
 };
 
 //File upload on s3 bucket
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
-});
+// const s3 = new AWS.S3({
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//     region: process.env.AWS_REGION,
+// });
 
 exports.uploadFilesTos3 = function (req, res) {
     const folderName = req.params['folderName'];
